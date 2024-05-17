3a2873b53a6c23e6a11035ef0229e7365d2c39f7
Bryctan <juanchoquintero1004@gmail.com>
2024-05-17 16:30:34 -0500

    visual design

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const rock = document.getElementById('btn-1');
 const paper = document.getElementById('btn-2');
 const scissors = document.getElementById('btn-3');
+let playerScore=0;
+let cpuScore=0;
 
 window.addEventListener("DOMContentLoaded", pressButton);
 
 function pressButton() {  
-    rock.addEventListener('click', (event)=> pressedRock(event,rock)),
-    paper.addEventListener('click', (event)=> pressedPaper(event,paper)),
-    scissors.addEventListener('click', (event)=> pressedScissors(event,scissors)
-);
+    rock.addEventListener('click', pressedRock),
+    paper.addEventListener('click', pressedPaper),
+    scissors.addEventListener('click', pressedScissors);
+}
+function pressedRock() {
+    result="rock"
+    return winCondition(result)
+}
+function pressedPaper() {
+    result="paper"
+    return winCondition(result)
+}
+function pressedScissors() {
+    result="scissors"
+    return winCondition(result)
 }
 
-function pressedRock(event, pos) {
-    let result="rock"
-    return result
+function winCondition(result) {
+    const options = ["rock", "paper", "scissors"]
+    const cpu= options[Math.floor(Math.random() * options.length)]
+    if (result === cpu ) {
+        console.log("empate")
+    } else if (
+        (result == "rock" && cpu =="paper") || (result == "paper" && cpu == "rock") || (result == "scissors" && cpu == "paper") 
+    ) {
+        playerScore+=1;
+        console.log("player win"+playerScore)
+    } else {
+        cpuScore+=1;
+        console.log("cpu win"+cpuScore)
+    }
 }
-function pressedPaper(event, pos) {
-    let result="paper"
-    return result
-}
-function pressedScissors(event, pos) {
-    let result="scissors"
-    return result
-}
-
-const options = ["rock", "paper", "scissors"]
-const cpu= options[Math.floor(Math.random() * options.length)]
-
